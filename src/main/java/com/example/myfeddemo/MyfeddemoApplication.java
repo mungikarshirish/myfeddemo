43cfabb07d49bad1b593d9191de39b1b0a9d0b1c
mungikarshirish <mungikarshirish15@gmail.com>
2026-03-10 17:32:28 +0530

    codeRefactor

---
+++ src/main/java/com/example/myfeddemo/MyfeddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfeddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfeddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfeddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myfeddemo/MyfeddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfeddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfeddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
